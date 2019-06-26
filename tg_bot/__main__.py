f178dae98c80129f3fa6441206bc1dcb68fd56e4
fravii848 <51963024+fravii848@users.noreply.github.com>
2019-06-26 15:37:13 -0300

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -20,7 +20,7 @@ from tg_bot.modules.helper_funcs.misc import paginate_modules
 PM_START_TEXT = """
 Hi {}, my name is *{}*! I'm a group manager bot maintained by [this](tg://user?id={}).
 
-[Source Code](http://github.com/PaulSonOfLars/tgbot)
+[Channel](http://t.me/fraviiu)
 You can find the list of available commands with /help
 """
 
